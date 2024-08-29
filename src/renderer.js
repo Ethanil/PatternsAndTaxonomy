d67378c5a3cd85badd6fadb0a9527debe8673906
Ethanil <69218171+Ethanil@users.noreply.github.com>
2024-08-29 12:29:42 +0200

    add mdi icons

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 // Vuetify
 import "vuetify/styles";
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
+
 const vuetify = createVuetify({
   components,
   directives,
